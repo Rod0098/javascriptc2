2e018f548911713c3e265315d477d70bec0c667e
Rod0098 <rodrigo8.00.rd@gmail.com>
2024-11-26 14:06:19 -0300

    exercicio concluido

--- controle/for1.js
+++ controle/for1.js
@@ -1,10 +1,14 @@
-/*let contador = 1
+let contador = 1
 while(contador <= 10) {  
     console.log(`contador = ${contador}`)
 contador++
-}*/
+}
  
-/*for(let i = 1; i <= 10; i++) {
+for(let i = 1; i <= 10; i++) {
     console.log(`i = ${i}`)
-}*/
-console.log('ola mundo')
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for(let i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`)
+}
