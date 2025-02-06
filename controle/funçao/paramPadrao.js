34c21a778099293867ea943ab13694c786473f34
Rod0098 <rodrigo8.00.rd@gmail.com>
2025-02-06 11:09:13 -0300

    parametro padrao atualizado

--- controle/funçao/paramPadrao.js
+++ controle/funçao/paramPadrao.js
@@ -14,7 +14,13 @@
     function padrao2(a, b, c) {
         a = a !== undefined ? a : 1
         b = 1 in arguments ? b : 1
-        c = isNaN(c) ? c : 1
+        c = isNaN(c) ? 1 : c
       return a + b + c
     }
     console.log(padrao2(), padrao2(2), padrao2(3, 4, 5))
+
+    //Estratégia mais fácil e atualizada pelo ES2015
+    function padrao3(a = 1, b = 1, c = 1) {
+      return a + b + c
+    }
+     console.log(padrao3(3, 4, 2))
